53eaa1b5ea025c2c8135cf06b5672d82e3caa2aa
sultampan <sultansyarif630@gmail.com>
2024-10-04 16:00:27 +0800

    daspro6

--- Pemilihan2Percobaan1_22.java
+++ Pemilihan2Percobaan1_22.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Pemilihan2Percobaan1_22 {
 
     public static void main(String[] args) {
@@ -7,13 +8,20 @@ public class Pemilihan2Percobaan1_22 {
         System.out.print("Masukkan Tahun: ");
         int tahun = input22.nextInt();
 
-            if ((tahun % 4) == 0) {
-                if ((tahun % 100) != 0)
+        if (tahun % 4 == 0) {
+            if (tahun % 100 == 0) {
+                if (tahun % 400 == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan tahun kabisat");
+                }
+            } else {
                 System.out.println("Tahun Kabisat");
-            else
-                 System.out.println("Bukan tahun kabisat");
-
-                 input22.close();
             }
+        } else {
+            System.out.println("Bukan tahun kabisat");
+        }
+
+        input22.close();
     }
 }
